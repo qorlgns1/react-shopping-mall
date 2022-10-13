db4397795d21fe222093f5206997a7738735ef08
qorlgns1 <rlgns0610@gmail.com>
2022-10-13 19:10:17 +0900

    design: 상품리스트 상하 패딩값 수정

--- src/components/ItemList/style.ts
+++ src/components/ItemList/style.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Wrapper = styled.ul`
   margin: 0 auto;
-  padding: 80px 0 180px;
+  padding: 80px 0 80px;
   max-width: 1280px;
   display: flex;
   flex-wrap: wrap;
@@ -10,7 +10,7 @@ const Wrapper = styled.ul`
   justify-content: space-evenly;
 
   @media screen and (max-width: 767px) {
-    padding: 40px 0 90px;
+    padding: 40px 0 40px;
     gap: 10px;
   }
 `;
