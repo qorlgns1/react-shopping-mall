d5e050e965fe317dc45e2603574e57417491da47
qorlgns1 <rlgns0610@gmail.com>
2022-10-28 18:13:15 +0900

    feat: 로고 클릭시 메인페이지로 이동하도록 기능 추가

--- src/components/header/BasicHeader/index.tsx
+++ src/components/header/BasicHeader/index.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { Wrapper, CustomSearchBar } from './style';
 import MainLogo from '../../common/logo/MainLogo';
 import Navbar from '../../common/navigation/NavBar';
+import { Link } from 'react-router-dom';
 
 export default function BasicHeader() {
   return (
     <Wrapper>
-      <MainLogo size={{ width: '128px', height: '38px' }} />
+      <Link to={'/'}>
+        <MainLogo size={{ width: '128px', height: '38px' }} />
+      </Link>
       <CustomSearchBar />
       <Navbar />
     </Wrapper>
