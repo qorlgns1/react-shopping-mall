3671608bbe6ac8c9fc4c40bf72a1063e69c915bc
qorlgns1 <rlgns0610@gmail.com>
2022-11-22 18:04:05 +0900

    refactor: 폴더명 앞에 공백이 있어서 제거 후 커밋

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Route, Switch } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import LoginPage from './pages/LoginPage';
 import JoinPage from './pages/JoinPage/index';
-import ProductDetailPage from './pages/\bProductDetailPage';
+import ProductDetailPage from './pages/ProductDetailPage';
 import ShoppingCartPage from './pages/ShoppingCartPage';
 
 function App() {
